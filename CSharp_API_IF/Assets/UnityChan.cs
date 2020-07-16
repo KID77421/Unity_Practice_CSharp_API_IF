0072ee0faa2e3496049afae5356b8aba1cda1b4e
KID <50666207+KID421@users.noreply.github.com>
2020-07-16 14:31:35 +0800

    完成

---
+++ CSharp_API_IF/Assets/UnityChan.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+
+namespace KID
+{
+    public class UnityChan : MonoBehaviour
+    {
+        private Animator ani;
+
+        private void Start()
+        {
+            ani = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            Move();
+        }
+
+        /// <summary>
+        /// 移動：按下 W 走路，放開恢復
+        /// </summary>
+        private void Move()
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                ani.SetBool("走路開關", true);
+            }
+            else if (Input.GetKeyUp(KeyCode.W))
+            {
+                ani.SetBool("走路開關", false);
+            }
+        }
+    }
+}
